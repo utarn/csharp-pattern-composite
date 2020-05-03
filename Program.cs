79d90b4e288fc7b34c3bffb60df05385c40f76f0
Utarn <utarn@yahoo.com>
2020-05-03 14:35:28 +0700

    Starter Project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pattern_composite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
